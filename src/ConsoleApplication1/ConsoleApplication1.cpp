e89cee6c33a2e394e951ec6aeb8d3358790a6096
Shannon Goh <sh4nnongoh@gmail.com>
2016-04-28 23:28:43 +0800

    Sample

---
+++ src/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,32 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include <iostream>
+
+#include "YahooAPIWrapper.h"
+
+int main()
+{
+	int temp;
+	const char* stock = "GOOG";
+	YahooAPIWrapper yahoo;
+
+	double bid = yahoo.GetBid(stock);
+	double ask = yahoo.GetAsk(stock);
+	const char* capi = yahoo.GetCapitalization(stock);
+
+	const char** bidAskCapi = yahoo.GetValues(stock, "b3b2j1");
+
+	std::cout << "Bid: " << bid << std::endl;
+	std::cout << "Ask: " << ask << std::endl;
+	std::cout << "Capi: " << capi << std::endl;
+
+	std::cout << "BidAskCapi[0]: " << bidAskCapi[0] << std::endl;
+	//std::cout << "BidAskCapi[1]: " << bidAskCapi[1] << std::endl;
+	//std::cout << "BidAskCapi[2]: " << bidAskCapi[2] << std::endl;
+
+	std::cin >> temp;
+}
+
