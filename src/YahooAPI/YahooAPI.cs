11fb7a66c55578407d006dab1043a9645085f251
Shannon Goh <sh4nnongoh@gmail.com>
2016-04-28 23:30:03 +0800

    Sample

---
+++ src/YahooAPI/YahooAPI.cs
@@ -0,0 +1,51 @@
+﻿using System.Net; // WebClient
+using System.Globalization; // CultureInfo
+
+namespace YahooAPI
+{
+    public class YahooAPI
+    {
+        private static readonly WebClient webClient = new WebClient();
+
+        private const string UrlTemplate = "http://finance.yahoo.com/d/quotes.csv?s={0}&f={1}";
+
+        private static double ParseDouble(string value)
+        {
+            return double.Parse(value.Trim(), CultureInfo.InvariantCulture);
+        }
+
+        private static string[] GetDataFromYahoo(string symbol, string fields)
+        {
+            // string request = string.Format(UrlTemplate, symbol, fields);
+
+            // string rawData = webClient.DownloadString(request).Trim();
+
+            // return rawData.Split(',');
+            return new string[]{"Some string from yahoo!" };
+        }
+
+        public double GetBid(string symbol)
+        {
+            //return ParseDouble(GetDataFromYahoo(symbol, "b3")[0]);
+            return 1.0;
+        }
+
+        public double GetAsk(string symbol)
+        {
+            //return ParseDouble(GetDataFromYahoo(symbol, "b2")[0]);
+            return 2.0;
+        }
+
+        public string GetCapitalization(string symbol)
+        {
+            //return GetDataFromYahoo(symbol, "j1")[0];
+            return "Get Capitalization";
+        }
+
+        public string[] GetValues(string symbol, string fields)
+        {
+            //return GetDataFromYahoo(symbol, fields);
+            return new string[] { "Some value from yahoo!" };
+        }
+    }
+}
---
+++ src/YahooAPIWrapper/YahooAPIWrapper.h
@@ -0,0 +1,20 @@
+#pragma once
+
+class YahooAPIWrapperPrivate;
+
+class __declspec(dllexport) YahooAPIWrapper
+{
+private: YahooAPIWrapperPrivate* _private;
+
+public: YahooAPIWrapper();
+
+public: ~YahooAPIWrapper();
+
+public: double GetBid(const char* symbol);
+
+public: double GetAsk(const char* symbol);
+
+public: const char* GetCapitalization(const char* symbol);
+
+public: const char** GetValues(const char* symbol, const char* fields);
+};
